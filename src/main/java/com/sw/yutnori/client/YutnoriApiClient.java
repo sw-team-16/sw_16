aa4abff99d4ab7368382254b647a0ede33ffc8c0
Junghyuk Lee <jbbnnom@gmail.com>
2025-05-08 17:13:12 +0900

    fix: URL mismatch with API endpoint

--- src/main/java/com/sw/yutnori/client/YutnoriApiClient.java
+++ src/main/java/com/sw/yutnori/client/YutnoriApiClient.java
@@ -3,40 +3,35 @@ package com.sw.yutnori.client;
 import com.sw.yutnori.dto.game.request.AutoThrowRequest;
 import com.sw.yutnori.dto.game.request.ManualThrowRequest;
 import com.sw.yutnori.dto.game.response.AutoThrowResponse;
-import com.sw.yutnori.dto.piece.response.MovablePieceResponse;
 import com.sw.yutnori.common.enums.YutResult;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
-import java.util.List;
 
 public class YutnoriApiClient implements GameApiClient {
 
     private final RestTemplate restTemplate;
-    private final String baseUrl = "http://localhost:8080/api";
+    private final String baseUrl = "http://localhost:8080";
 
     public YutnoriApiClient() {
         this.restTemplate = new RestTemplate();
     }
 
+    // S2-1 : 윷 랜덤 던지기
+    @Override
     public AutoThrowResponse getRandomYutResult(Long gameId, Long turnId, Long playerId) {
         AutoThrowRequest request = new AutoThrowRequest( playerId);
 
-        String url = baseUrl + "/game/" + gameId + "/turn/random";
+        String url = baseUrl + "/api/game/" + gameId + "/turn/random/throw";
         return restTemplate.postForObject(url, request, AutoThrowResponse.class);
     }
 
+    // S2-2 : 수동(지정) 윷 던지기
+    @Override
     public void throwYutManual(Long gameId, Long turnId, Long playerId, Long pieceId, YutResult result) {
         ManualThrowRequest request = new ManualThrowRequest( playerId, pieceId, result);
 
-        String url = baseUrl + "/game/" + gameId + "/turn/manual";
+        String url = baseUrl + "/api/game/" + gameId + "/turn/manual/throw";
         restTemplate.postForObject(url, request, Void.class);
     }
 
-    // 말 관련 - 별도 구현 필요
-    public List<MovablePieceResponse> getMovablePieces(Long playerId) {
-        String url = baseUrl + "/game/player/" + playerId + "/movable-pieces";
-        MovablePieceResponse[] response = restTemplate.getForObject(url, MovablePieceResponse[].class);
-        return response != null ? List.of(response) : Collections.emptyList();
-    }
+    // 이하 나머지 API 클라이언트 구현 필요
 }
