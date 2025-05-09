b87a18f0a56affaf8afe05bb8366a538c97e6536
Hong-Iron <hongcheolmin004@gmail.com>
2025-05-09 23:59:50 +0900

    Ui 수정 이전 백업용 커밋

--- src/main/java/com/sw/yutnori/service/PieceService.java
+++ src/main/java/com/sw/yutnori/service/PieceService.java
@@ -1,7 +1,9 @@
 package com.sw.yutnori.service;
 
 import com.sw.yutnori.dto.game.request.MovePieceRequest;
+import com.sw.yutnori.dto.game.response.GameStatusResponse;
 
 public interface PieceService {
     void movePiece(Long gameId, MovePieceRequest request);
 }
+
