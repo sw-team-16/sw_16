e82d9b2e7a9e4d0fdc820d0c8af589e50834c453
jpotw <junupark1@cau.ac.kr>
2025-05-09 21:43:59 +0900

    simplify pathnode logic

--- src/main/java/com/sw/yutnori/domain/Piece.java
+++ src/main/java/com/sw/yutnori/domain/Piece.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.sw.yutnori.repository.PathNodeRepository;
+import com.sw.yutnori.domain.Board;
+import com.sw.yutnori.domain.PathNode;
 
 @Entity
 @Getter
@@ -21,11 +24,11 @@ public class Piece {
     @JoinColumn(name = "player_id", nullable = false)
     private Player player;
 
-    @Column(name = "xcoord", nullable = false)
-    private int x;
+    @Column(name = "a", nullable = false)
+    private int a;
 
-    @Column(name = "ycoord",nullable = false)
-    private int y;
+    @Column(name = "b", nullable = false)
+    private int b;
 
     @Column(nullable = false)
     private boolean isFinished = false;
@@ -35,8 +38,21 @@ public class Piece {
 
     private Long groupId;
 
-    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PieceState state = PieceState.READY;
 
+    /**
+     * (a, b): 논리 좌표 (메인 로직/DB용)
+     * (x, y): 실제 좌표 (FE 렌더링용, FE에서만 사용)
+     */
+    public void setLogicalPosition(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int getA() {
+        return a;
+    }
+    public int getB() {
+        return b;
+    }
 }
